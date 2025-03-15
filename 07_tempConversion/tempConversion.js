07a6899d97706b8d3010ed852c1b33f7c5733a74
DocQuantic <35266773+DocQuantic@users.noreply.github.com>
2025-03-15 18:30:20 +0100

    Solve exercise 07_tempConversion

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(farenheitTemp) {
+  const celsiusTemp = (farenheitTemp-32)/1.8;
+
+  return parseFloat(celsiusTemp.toFixed(1));
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celiusTemp) {
+  const farenheitTemp = celiusTemp*1.8+32;
+
+  return parseFloat(farenheitTemp.toFixed(1));
 };
 
 // Do not edit below this line
