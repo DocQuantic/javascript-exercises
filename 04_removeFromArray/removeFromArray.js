6b8bbb161bb5d1097253678ba242eaf31061ee6d
DocQuantic <35266773+DocQuantic@users.noreply.github.com>
2025-03-15 15:23:30 +0100

    Solve exercise 04_removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,13 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, elementsToRemove) {
+    for(let i=1; i<arguments.length; i++){
+        for(let j=array.length; j>=0; j--){
+            if(array[j]===arguments[i]){
+                array.splice(j, 1);
+            }
+        }
+    }
+
+    return array;
 };
 
 // Do not edit below this line
