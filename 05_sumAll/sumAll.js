d3b42202f3683a16012a5a6a2161a08525343c15
DocQuantic <35266773+DocQuantic@users.noreply.github.com>
2025-03-15 15:38:14 +0100

    Solve exercise 05_sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,15 @@
-const sumAll = function() {
+const sumAll = function(numMin, numMax) {
+    let sum = 0;
 
+    if(numMin<0 || numMax <0 || !Number.isInteger(numMin) || !Number.isInteger(numMax) || typeof(numMin) !== "number" || typeof(numMax) !== "number"){
+        return "ERROR";
+    }
+
+    for(let i=Math.min(numMin, numMax); i<=Math.max(numMin, numMax); i++){
+        sum += i;
+    }
+
+    return sum;
 };
 
 // Do not edit below this line
