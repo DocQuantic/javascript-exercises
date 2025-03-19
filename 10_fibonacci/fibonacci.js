71d8816dec3b65810f32cc449159b647b45f3e83
DocQuantic <35266773+DocQuantic@users.noreply.github.com>
2025-03-19 12:22:34 +0100

    Solve 10_plaindromes

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,13 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+    if(num < 0){
+        return "OOPS";
+    }
+    fib = [0, 1];
+    for(let i=2; i<=parseInt(num); i++){
+        fib[i] = fib[i-1] + fib[i-2];
+    }
 
+    return fib[parseInt(num)];
 };
 
 // Do not edit below this line
