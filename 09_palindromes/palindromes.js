00ea7e03e33147db695e95371fb8c03077a2d67d
DocQuantic <35266773+DocQuantic@users.noreply.github.com>
2025-03-19 12:12:11 +0100

    Solve 09_palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,9 @@
-const palindromes = function () {
+const palindromes = function (string) {
+    simpleString = string.replaceAll("!", "").replaceAll(",", "").replaceAll(".", "").replaceAll(" ", "").toLowerCase();
+    console.log(simpleString)
 
+    revertString = simpleString.split("").reverse().join("");
+    return simpleString === revertString;
 };
 
 // Do not edit below this line
